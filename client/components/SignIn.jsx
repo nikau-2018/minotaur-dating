2cdf7d6bdf19e61be51ccf8d737587bbec348f44
Amy Han <syamyhan@gmail.com>
2018-08-31 10:58:50 +1200

    updated registration, resgistrationButtons,Signin componenets

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
-const SignIn = () => {
+const SignIn = (props) => {
   return (
-    
+    <div>
+      <form>
+        <label>
+          Name
+          <input type = "test" name="name" />
+        </label>
+        <label>
+          Email
+          <input type = "test" name="email" />
+        </label>
+        <label>
+          Age
+          <input type = "test" name="age" />
+        </label>
+        <button
+          type= "button"
+          onClick = {() => props.goTo('Registration')
+        </button>
+      </form>
+    </div>
   )
 }
 
 export default SignIn
-
