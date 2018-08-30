b4ed4cedeba5b0faee3d0bc005d2a69bfa0f42ac
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-08-31 11:44:54 +1200

    Working on getting data through API

---
+++ client/apiClient.js
@@ -0,0 +1,19 @@
+import request from 'superagent'
+
+const rootUrl = 'http://192.168.1.156:3000/api'
+
+export function getAllUsers () {
+  return request.get(rootUrl + '/getAllUsers')
+    .then(res => {
+      console.log(res.body.users)
+      return res.body.users
+    })
+}
+
+export function getUser () {
+  return request.get(rootUrl + '/getUser/99902')
+    .then(res => {
+      console.log('res.body.user', res.body.user)
+      return res.body.user
+    })
+}
--- client/components/Others.jsx
+++ client/components/Others.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import {getUser} from '../apiClient'
 
-const Others = () => {
-  return (
-    <div>
-      <h1>Testing Others</h1>
-    </div>
-  )
+class Others extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: []
+    }
+  }
+
+  componentDidMount () {
+    getUser()
+      .then(user => {
+        this.setState({user})
+      })
+  }
+
+  render () {
+    console.log('this.state.user', this.state.user.name)
+    return (
+      <div className='others'>
+        <h1>Testing Others API</h1>
+        <div> {this.state.user.name}
+          {/*           {this.state.users.map(user => (
+            <div key={user.id}>{user.name}</div>
+          ))} */}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Others
