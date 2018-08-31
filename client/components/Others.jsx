746ece3df46c2483d80f752b61fa54deba24035c
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-08-31 13:28:13 +1200

    others now showing multiple users

--- client/components/Others.jsx
+++ client/components/Others.jsx
@@ -1,39 +1,29 @@
 import React from 'react'
-import {getUser/* , getAllUsers */} from '../apiClient'
+import {getAllUsers} from '../apiClient'
 
 class Others extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      user: []
-    }
-/*     this.state = {
       users: []
-    } */
+    }
   }
 
   componentDidMount () {
-    getUser()
-      .then(user => {
-        this.setState({user})
+    getAllUsers()
+      .then(users => {
+        this.setState({users})
       })
-/*     getAllUsers()
-    .then(user => {
-      this.setState({users})
-    }) */
   }
 
   render () {
-    console.log('this.state.user', this.state.user.name)
+    // console.log('this.state.user', this.state.user.name)
     return (
       <div className='others'>
         <h1>Testing Others API</h1>
-        {/* Individual user */}
-        <div> {this.state.user.name}</div>
-          {/* All users          {this.state.users.map(user => (
-            <div key={user.id}>{user.name}</div>
-          ))} */}
-        </div>
+        {this.state.users.map(users => (
+          <div key={users.id}>{users.name}</div>
+        ))}
       </div>
     )
   }
