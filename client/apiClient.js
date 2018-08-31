21e095941595e3d537a1c07ba01aa1eb40ba3e83
georgehardy <george.hardy@gmail.com>
2018-09-01 10:10:00 +1200

    change API to localhost

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,11 +1,10 @@
 import request from 'superagent'
 
-const rootUrl = 'http://192.168.1.156:3000/api'
+const rootUrl = 'localhost:3000/api'
 
 export function getAllUsers () {
   return request.get(rootUrl + '/getAllUsers')
     .then(res => {
-      console.log(res.body.users)
       return res.body.users
     })
 }
@@ -14,23 +13,14 @@ export function getUser (id) {
   const reqStr = `getUser${id}`
   return request.get(rootUrl + reqStr)
     .then(res => {
-      console.log('res.body.user', res.body.user)
       return res.body.user
     })
 }
 
 export function addUser (userData) {
-  const testData = {
-    name: 'TestName2',
-    email: 'TestEmail',
-    password: 'TestPassword',
-    age: 'TestAge',
-    bio: 'TestBio'
-  }
   request
     .post(rootUrl + '/addUser')
     .set('Content-Type', 'application/json')
     .send(userData)
-    // .send(testData)
     .end()
 }
