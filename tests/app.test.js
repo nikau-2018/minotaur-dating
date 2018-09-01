410854f5be57318cfedaa98f800cb16e4db82b9f
georgehardy <george.hardy@gmail.com>
2018-09-01 13:33:05 +1200

    comment out unused tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import {shallow} from 'enzyme'
+// import React from 'react'
+// import {shallow} from 'enzyme'
 
-import App from '../client/components/App'
+// import App from '../client/components/App'
 
-test('<App />', () => {
-  const expected = 'React development has begun!'
-  const wrapper = shallow(<App />)
-  expect(wrapper.text()).toBe(expected)
-})
+// test('<App />', () => {
+//   const expected = 'React development has begun!'
+//   const wrapper = shallow(<App />)
+//   expect(wrapper.text()).toBe(expected)
+// })
